de411dcd59b2c8a71d3aed84c6cae879ef54652d
GarethAMarks <gam54@cam.ac.uk>
2024-10-14 13:28:26 +0100

    Modify make script so it will create obj/Debug if needed

--- make_BSEvolver_gcc.sh
+++ make_BSEvolver_gcc.sh
@@ -1,3 +1,5 @@
+mkdir obj
+mkdir obj/Debug
 g++ -Wall -fexceptions -g  -c "mathutils.cpp" -o obj/Debug/mathutils.o
 g++ -Wall -fexceptions -g  -c "BosonStar.cpp" -o obj/Debug/BosonStar.o
 g++ -Wall -fexceptions -g  -c "EvolutionVariables.cpp" -o obj/Debug/EvolutionVariables.o
